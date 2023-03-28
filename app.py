3af81997e5fe69ba99fe10ae357fb79119a37c6a
Sean <shroby30@gmail.com>
2023-03-27 20:46:23 -0400

    Datapages linked together with flask.

--- app.py
+++ app.py
@@ -5,5 +5,34 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
-def homepage():
-    return render_template('mainPage.html', title='Home')
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
+
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+@app.route("/resetPassword")
+def reset():
+    return render_template('resetPassword.html')
+
+
+@app.route("/reset")
+def forgot():
+    return render_template('reset.html')
+
+
+@app.route("/data")
+def dataPage():
+    return render_template('data.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
