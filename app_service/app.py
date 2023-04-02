180eeb66952099ff774c30a51b47ac5a001a320e
SisoroT <rtaylor6410@gmail.com>
2023-04-02 17:52:44 -0400

    added logic to /home that gets the repo and user from forms on home.html, queries gh api, and passes all data to /data route. /data route now takes in necessary params

--- app_service/app.py
+++ app_service/app.py
@@ -1,17 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
+from backend.gh_api import send_request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    error = None
+
+    # if the form is submitted
+    if request.method == "POST":
+        repo = request.form["repository"]
+        author = request.form["username"]
+
+        # if both repository and username have been filled out
+        if repo and author:
+            endpoints = ["commits", "pulls", "reviews", "comments"]
+            counts = {}
+
+            for endpoint in endpoints:
+                data = send_request(repo, author, endpoint)
+
+                # if total_count isn't in the response, the repo or username is invalid
+                if "total_count" not in data:
+                    error = "Invalid repository or username. Please check the input and try again."
+                    return render_template("home.html", error=error)
+
+                counts[endpoint] = data["total_count"]
+
+            user_not_contributor = sum(counts.values()) == 0
+            if user_not_contributor:
+                error = (
+                    f'User "{author}" is not a contributor to the "{repo}" repository.'
+                )
+                return render_template("home.html", error=error)
+
+            # if the repo and username are valid, redirect to the data page
+            # and pass all the information received from the API
+            return redirect(
+                url_for(
+                    "data",
+                    repository=repo,
+                    username=author,
+                    commits=counts["commits"],
+                    pulls=counts["pulls"],
+                    reviews=counts["reviews"],
+                    comments=counts["comments"],
+                )
+            )
+        else:
+            print(error)
+            error = "Please provide both repository and username."
+
+    return render_template("home.html", error=error)
 
 
-@app.route("/data")
-def data():
-    return render_template("data.html")
+@app.route(
+    "/data/<path:repository>/<string:username>/<int:commits>/<int:pulls>/<int:reviews>/<int:comments>"
+)
+def data(repository, username, commits, pulls, reviews, comments):
+    return render_template(
+        "data.html",
+        repository=repository,
+        username=username,
+        commits=commits,
+        pulls=pulls,
+        reviews=reviews,
+        comments=comments,
+    )
 
 
 @app.route("/login")
