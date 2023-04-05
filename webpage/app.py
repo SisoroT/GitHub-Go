52c3ef5207f72dd32a689f6c295c3f08a36a98ab
SisoroT <rtaylor6410@gmail.com>
2023-04-05 17:53:03 -0400

    added integration with api key from form and cleaned code

--- webpage/app.py
+++ webpage/app.py
@@ -1,41 +1,47 @@
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request, flash, session
 from backend.gh_api import send_request
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key_here"
+app.secret_key = "super_secret_key"
 
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home():
-    error = None
-
     # if the form is submitted
     if request.method == "POST":
+        # get inputs from forms
         repo = request.form["repository"]
         author = request.form["username"]
+        api_key = request.form["api_key"]
+
+        # Store the API key in the session
+        session["api_key"] = api_key
+
+        template_params = {
+            "repository": repo,
+            "username": author,
+            "api_key": api_key,
+        }
 
         # if both repository and username have been filled out
-        if repo and author:
+        if repo and author and api_key:
             endpoints = ["commits", "pulls", "reviews", "comments"]
             counts = {}
 
             for endpoint in endpoints:
-                try:
-                    response = send_request(repo, author, endpoint)
-                except Exception as e:
-                    flash(str(e))
-                    return render_template(
-                        "home.html", repository=repo, username=author
-                    )
+                response = send_request(repo, author, api_key, endpoint)
+
+                is_error = isinstance(response, str)
+                if is_error:
+                    flash(response)
+                    return render_template("home.html", **template_params)
 
                 # if total_count isn't in the response, the repo or username is invalid
                 if "total_count" not in response:
                     error = "Invalid repository or username. Please check the input and try again."
                     flash(error)
-                    return render_template(
-                        "home.html", repository=repo, username=author
-                    )
+                    return render_template("home.html", **template_params)
 
                 counts[endpoint] = response["total_count"]
 
@@ -45,7 +51,7 @@ def home():
                     f'User "{author}" is not a contributor to the "{repo}" repository.'
                 )
                 flash(error)
-                return render_template("home.html", repository=repo, username=author)
+                return render_template("home.html", **template_params)
 
             # if the repo and username are valid, redirect to the data page
             # and pass all the information received from the API
@@ -61,11 +67,12 @@ def home():
                 )
             )
         else:
-            error = "Please provide both repository and username."
+            error = "Please provide a repository, username, and api key."
             flash(error)
-            return render_template("home.html", repository=repo, username=author)
+            return render_template("home.html", **template_params)
 
-    return render_template("home.html")
+    api_key = session.get("api_key", "")
+    return render_template("home.html", api_key=api_key)
 
 
 @app.route("/data/<path:repository>/<string:username>")
