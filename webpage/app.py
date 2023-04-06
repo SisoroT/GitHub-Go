69b92f23c3832d555bf1bd8be78c418cec6a0b91
TalaalTahir <77017392+TalaalTahir@users.noreply.github.com>
2023-04-06 16:12:46 -0400

    Added database.

--- webpage/app.py
+++ webpage/app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True,nullable=False)
+    email = db.Column(db.String(120), unique=True,nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    searches = db.relationship('Search', backref='user',lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.password}')"
+
+class Search(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    repo = db.Column(db.String(30), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    
+    def __repr__(self):
+        return f"Search('{self.username}','{self.repo}')"
 
 @app.route("/")
 @app.route("/home")
@@ -36,3 +58,4 @@ def data():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
