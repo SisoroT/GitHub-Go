e4f33baee457f581191ea532de563d35eac1bb3f
kmwiegand <122555195+kmwiegand@users.noreply.github.com>
2023-04-13 19:01:27 -0400

    Login button now takes form data and passes to DB
    register.py:
    *updated code to arguments in function call to pass user and pass
    *passes arguments to sql db directly (not test strings)
    
    login.py:
    *updated code to arguments in function call to pass user and pass
    *passes arguments to sql db directly (not test strings)
    
    app.py:
    *updated login render to take POST data when form is loaded
    *redirects to the home page when finished
    
    login.html:
    *changed form action to POST

--- webpage/login.py
+++ webpage/login.py
@@ -2,53 +2,55 @@ from sqlalchemy import create_engine, Column, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+def searchLogin(searchUser, searchPass):
 
-#Example login and password
-givenUser = "fakeUser"
-givenPass = "hunter2"
+    Base = declarative_base()
 
-#Registration object. 
-class Register(Base):
-    __tablename__ = "Logins"
+    #Example login and password
+    givenUser = searchUser
+    givenPass = searchPass
 
-    #Username and Password columns defined. These make up the 2 columns of our SQL database
-    uName = Column("Username", String, primary_key=True)
-    password = Column("Password", String)
+    #Registration object. 
+    class Register(Base):
+        __tablename__ = "Logins"
 
-    #object constructor
-    def __init__(self, uName, password):
-        self.uName = uName
-        self.password = password
-    #return function of object as string
-    def __repr__(self):
-        return f"({self.uName}) ({self.password})"
-    
-#SQL engine that converts python code to SQL code. Creates mydb.db file. 
-#Use SQLite Viewer extention to view accounts.db contents.
-engine = create_engine("sqlite:///accounts.db", echo=True)
-Base.metadata.create_all(bind=engine)
+        #Username and Password columns defined. These make up the 2 columns of our SQL database
+        uName = Column("Username", String, primary_key=True)
+        password = Column("Password", String)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+        #object constructor
+        def __init__(self, uName, password):
+            self.uName = uName
+            self.password = password
+        #return function of object as string
+        def __repr__(self):
+            return f"({self.uName}) ({self.password})"
+        
+    #SQL engine that converts python code to SQL code. Creates mydb.db file. 
+    #Use SQLite Viewer extention to view accounts.db contents.
+    engine = create_engine("sqlite:///accounts.db", echo=True)
+    Base.metadata.create_all(bind=engine)
 
-#If the variables for the passed username and passed password are not null or length == 0
-#search the database for the username. 
-#if the username is not found print username not found
-#if the username is found, check if the given password matches.
-if givenUser and givenPass:
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    results = session.query(Register).filter(Register.uName == givenUser).all()
-    
-    if len(results)>0:
-        print("Username Found")
-        for f in results:
-            if f.password == givenPass:
-                print(f.uName + " logged in")
-            else:
-                print("INCORRECT PASSWORD")
-    else:
-        print("USERNAME NOT FOUND")
+    #If the variables for the passed username and passed password are not null or length == 0
+    #search the database for the username. 
+    #if the username is not found print username not found
+    #if the username is found, check if the given password matches.
+    if givenUser and givenPass:
 
-elif not givenPass or not givenUser:
-    print("User or password null")
+        results = session.query(Register).filter(Register.uName == givenUser).all()
+        
+        if len(results)>0:
+            print("Username Found")
+            for f in results:
+                if f.password == givenPass:
+                    print(f.uName + " logged in")
+                else:
+                    print("INCORRECT PASSWORD")
+        else:
+            print("USERNAME NOT FOUND")
+
+    elif not givenPass or not givenUser:
+        print("User or password null")
